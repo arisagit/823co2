1cde4947810c4c7e428bff32bbc653407b27a2c9
oldpnd <3oldpond5@gmail.com>
2023-08-23 11:27:10 +0900

    plot

--- plot.R
+++ plot.R
@@ -8,7 +8,7 @@ df3<-mutate(df3,gdp_per_capita = gdp/population)
 df3
 
 df3 %>%
-  filter(country %in% c(""))
+  filter(country %in% c("Canada", "France", "Germany", "Italy", "Japan", "United Kingdom", "United States"))%>%
   group_by(country) %>% 
   ggplot(mapping = aes(x = gdp_per_capita, y = co2_per_capita))+
   geom_point()

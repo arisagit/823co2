f6ed7a1b047f26152c4b211c0419eb8c9fc24b79
arisagit <a.pompom18@gmail.com>
2023-08-23 11:01:30 +0900

    test

--- plot.R
+++ plot.R
@@ -4,4 +4,6 @@ names(df)
 df2 <- df[,c("country", "year", "population", "gdp", "co2_per_capita")]
 df3 <- filter(df2,!is.na(gdp))
 library(ggplot2)
-gdp_per_capita = "gdp"/"population"
+df3<-mutate(df3,gdp_per_capita = gdp/population)
+df3
+df3%/%group_by(country)

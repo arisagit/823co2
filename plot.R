5ebb7a0b2c8565e2c620c462d6ae282ed6fff362
yuma-0619 <iwata-yuma149@g.ecc.u-tokyo.ac.jp>
2023-08-23 11:16:39 +0900

    plot commit

--- plot.R
+++ plot.R
@@ -6,4 +6,9 @@ df3 <- filter(df2,!is.na(gdp))
 library(ggplot2)
 df3<-mutate(df3,gdp_per_capita = gdp/population)
 df3
-df3%/%group_by(country)
+
+df3 %>%
+  filter(country %in% c(""))
+  group_by(country) %>% 
+  ggplot(mapping = aes(x = gdp_per_capita, y = co2_per_capita))+
+  geom_point()
